de475c638e43b4dbd4713b4d6fce197391311d65
Tpicoulegiusta <117988938+Tpicoulegiusta@users.noreply.github.com>
2025-01-13 16:22:41 +0100

    Add files via upload

---
+++ inception/srcs/inception/srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Lire les secrets
+if [ -f "/run/secrets/db_password" ]; then
+	. /run/secrets/db_password
+fi
+
+if [ -f "/run/secrets/credentials" ]; then
+    . /run/secrets/credentials
+fi
+
+mysql_install_db --user=root --datadir=/var/lib/mysql
+
+mysqld_safe --user=root --datadir=/var/lib/mysql &
+
+# Attendre que la base de données soit accessible
+until mysqladmin ping >/dev/null 2>&1; do
+	sleep 1
+done
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' WITH GRANT OPTION;"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysqladmin shutdown
+
+mysqld_safe --user=root --datadir=/var/lib/mysql
